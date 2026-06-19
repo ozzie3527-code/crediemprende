d5a7b5c1f60906a1663564ed0070f717cf65cd6e
ozzie3527-code <ozzie3527@gmail.com>
2026-06-19 14:39:04 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 // config.php
 
 // Configuración de las credenciales de Telegram
-define('TELEGRAM_BOT_TOKEN', '8258529771:AAH5UT6lHZo0G8Nb3jR3U1tZABNwbuCy_Yw');
+define('TELEGRAM_BOT_TOKEN', '8258529771:AAFGnRueuW-IzgLJciQydyNBNFAUjYfrJxU');
 define('TELEGRAM_CHAT_ID', '7517275943');
 ?>
